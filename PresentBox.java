23ca90dc0ae0ff92cb520a1da0d1bf94d0667cd6
Olga804 <54376627+Olga804@users.noreply.github.com>
2019-08-24 22:49:02 +0300

    24/08/2019/2248

--- PresentBox.java
+++ PresentBox.java
@@ -7,10 +7,12 @@ public class PresentBox implements Box {
 
     @Override
     public void add(Sweet sweet) {
-        int d=sweets.length;
-       Sweet[] copy= Arrays.copyOf(sweets, d+1);
-        copy[d]=sweet;
-        sweets=Arrays.copyOf(copy, d+1);
+        if (sweet!=null) {
+            int d = sweets.length;
+            Sweet[] copy = Arrays.copyOf(sweets, d + 1);
+            copy[d] = sweet;
+            sweets = Arrays.copyOf(copy, d + 1);
+        }
     }
 
     @Override
@@ -35,26 +37,37 @@ public class PresentBox implements Box {
     public void delete(int index) {
         for (int i=index;i<sweets.length-1;i++){
             sweets[i]=sweets[i+1];
-           Sweet [] copy= Arrays.copyOf(sweets,sweets.length-1);
-            sweets=Arrays.copyOf(copy, copy.length);
 
         }
+        Sweet [] copy= Arrays.copyOf(sweets,sweets.length-1);
+        sweets=Arrays.copyOf(copy, copy.length);
 
     }
 
     @Override
     public void optimizeWeight(double maxWeight) {
-        while (getBoxWeight()>maxWeight){
-            double min=sweets[0].weight;
-            int index=0;
-            for (int i=0;i<sweets.length;i++){
-                if (sweets[i].weight<min){
-                    min=sweets[i].weight;
-                    index=i;
+
+        while (getBoxWeight()>maxWeight) {
+            double min = sweets[0].weight;
+            int index = 0;
+            for (int i = 0; i < sweets.length; i++) {
+                if (sweets[i].weight < min) {
+                    min = sweets[i].weight;
+                    index = i;
+                }
+            }
+            if(sweets.length==2){
+                if(index==0&&sweets[1].weight>maxWeight&&sweets[0].weight<maxWeight){
+                    index=1;
+                } else {
+                    if(index==1&&sweets[0].weight>maxWeight&&sweets[1].weight<maxWeight){
+                        index=0;
+                    }
                 }
             }
             delete(index);
         }
+
 
     }
 
@@ -67,6 +80,15 @@ public class PresentBox implements Box {
                 if (sweets[i].price<min){
                     min=sweets[i].price;
                     index=i;
+                }
+            }
+            if(sweets.length==2){
+                if(index==0&&sweets[1].price>maxPrice&&sweets[0].price<maxPrice){
+                    index=1;
+                } else {
+                    if(index==1&&sweets[0].price>maxPrice&&sweets[1].price<maxPrice){
+                        index=0;
+                    }
                 }
             }
             delete(index);
